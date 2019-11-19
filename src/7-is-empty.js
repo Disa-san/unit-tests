7922e0522cc2dd1733126d1c9c9f40815ae052c6
Disa de Beau <Disan@Disas-MacBook-Air.local>
2019-11-19 17:50:12 +0100

    Still working on 8 and 9

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,18 @@
+import { objectExpression } from "@babel/types"
+
 export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
 
   if (type === 'string') {
-    return stringArrayOrObject === ''
+    return stringArrayOrObject === ""
   }
 
-  return false
+  if (type === "array") {
+    return stringArrayOrObject.length === 0
+  }
+
+  if (type === "object") {
+    return Object.entries(stringArrayOrObject).length === 0
+  }
+
 }
